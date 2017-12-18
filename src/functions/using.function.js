d257e3823c443f42434cf0add02acd837a381500
Rick Hopkins <rick.hopkins@gmail.com>
2017-12-18 11:34:14 -0500

    Updated the using function so that it will work with class objects.

--- src/functions/using.function.js
+++ src/functions/using.function.js
@@ -9,16 +9,30 @@ function using(obj, code, ref, refData) {
 		{ entity: '&#10;', char: '\n' },
 		{ entity: '&#9;', char: '\t' }
 	];
-
 	decodingMap.forEach(m => {
 		code = code.replace(m.entity, m.char);
 	});
 
+	/** get the property and method names list */
+	let properties = Object.getOwnPropertyNames(obj);
+	let proto = Object.getPrototypeOf(obj);
+	let methods = Object.getOwnPropertyNames(proto);
+
+	/** create list of private methods to remove */
+	let removedMethods = ['constructor', '__defineGetter__', '__defineSetter__', 'hasOwnProperty', '__lookupGetter__', '__lookupSetter__', 'isPrototypeOf', 'propertyIsEnumerable', 'toString', 'valueOf', '__proto__', 'toLocaleString'];
+	methods.forEach(m => m.startsWith('_') ? removedMethods.push(m) : null);
+	removedMethods.forEach(rm => methods.splice(methods.indexOf(rm), 1));
+
+	/** concatenate the properties and methods */
+	let propsAndMethods = properties.concat(methods);
+
+	/** create a function for executing the given code */
 	let withFunc = new Function('obj', ref, `
-		let { ${Object.getOwnPropertyNames(obj).join(', ') } } = obj;
+		let { ${propsAndMethods.join(', ')} } = obj;
 		return (${code});
 	`);
 
+	/** execute the function */
 	return withFunc(obj, refData);
 }
 
